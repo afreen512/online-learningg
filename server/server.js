1cac24cd76053af5c51fe51eb252895216ec0bf9
afreen512 <afreenhussain866@gmail.com>
2020-07-12 23:25:10 +0500

    depoly

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const path =require('path');
 
 const app = express();
 
@@ -22,7 +23,10 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(cors({ origin: process.env.CLIENT_URL }));
 if (process.env.NODE_ENV=='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) =>{
+     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
 
 }
 // middlewares
