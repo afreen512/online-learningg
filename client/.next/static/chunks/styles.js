45042b299ff925b7d5c6ed750c0fe3f75ae96da5
Arijy <arijchaudhary@gmail.com>
2020-07-14 15:57:50 +0500

    reset

--- client/.next/static/chunks/styles.js
+++ client/.next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1594665097684");
+            injectCss(link, link.href.split("?")[0] + "?unix=1594724044606");
           });
       }
     }
--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8973aed9752e8aed4c83";
+/******/ 	var hotCurrentHash = "9cf7977f4942192b38c7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
