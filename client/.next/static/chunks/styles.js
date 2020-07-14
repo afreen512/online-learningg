2b95d25e0b638bc56aa67732ec0129df8f9865c0
nazle786 <64862781+nazle786@users.noreply.github.com>
2020-07-14 08:33:28 -0700

    commit

--- client/.next/static/chunks/styles.js
+++ client/.next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1594719735716");
+            injectCss(link, link.href.split("?")[0] + "?unix=1594739818340");
           });
       }
     }
--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2a32207d1baea0db9745";
+/******/ 	var hotCurrentHash = "0e6bf2931fdc1819f189";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
