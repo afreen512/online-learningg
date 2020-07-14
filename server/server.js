8d87482aae5f37f80510cf2dbea5079d612dc3c9
afreen512 <afreenhussain866@gmail.com>
2020-07-14 11:03:23 +0500

    verfication

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const path =require('path');
 
 const app = express();
 
@@ -22,16 +21,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use(cors({ origin: process.env.CLIENT_URL }));
-if (process.env.NODE_ENV=='production'){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res) =>{
-     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-    });
 
-}
 // middlewares
 app.use('/api', authRoutes);
-
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
